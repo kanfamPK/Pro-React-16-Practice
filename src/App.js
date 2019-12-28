abbf576007c35e22a2fb0436fb40b4cd90574669
kpham32 <kpham32@dxc.com>
2019-12-28 13:22:40 +0700

    28/12/2019 commit 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "Khan",
+      key1: "january"
+    }
+  }
+  randomNum = ()=>{
+    let a = Math.random();
+    this.setState((state,props)=>{
+      return({key1: a});
+    });
+  }
+  // randomNum(){
+  //   let a = Math.random();
+  //   this.setState((state,props)=>{
+  //     return({key1: a});
+  //   });
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        <h4 className="bg-primary text-white text-center p-2">
+          {this.state.userName}'s Todo List
+        </h4>
+        <p className="font-weight-bold">
+          This is state's key1 : {this.state.key1}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <button className="bg-danger p-3 text-white" onClick={this.randomNum}>
+          Click to change state's key1
+        </button>
+      </div>
+    )
+  }
 }
 
 export default App;
